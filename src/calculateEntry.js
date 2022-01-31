21f5d74c77d7b7625c95ced1c878751ad1020b05
Yuut <ingride.reis.b@gmail.com>
2022-01-30 23:19:27 -0300

    requisito 9 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const visitante = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((element) => {
+    switch (true) {
+    case element.age < 18:
+      visitante.child += 1;
+      break;
+    case element.age < 50:
+      visitante.adult += 1;
+      break;
+    default: visitante.senior += 1;
+    }
+  });
+  return visitante;
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || JSON.stringify(entrants) === '{}') {
+    return 0;
+  }
+  const number = countEntrants(entrants);
+  return number.child * prices.child + number.adult * prices.adult + number.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((element) => element.id === id);
+  const firstAnimal = species.find((element) => element.id === funcionario.responsibleFor[0]);
+  const sortAnimal = firstAnimal.residents.sort((ord1, ord2) => ord2.age - ord1.age);
+  return Object.values(sortAnimal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
