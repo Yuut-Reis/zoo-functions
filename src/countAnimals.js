db40cb58b4508b9eea733d628da8838d78c65d1a
Yuut <ingride.reis.b@gmail.com>
2022-01-30 21:10:17 -0300

    requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui aaa
+  if (!animal) {
+    const result = {};
+    species.forEach((element) => {
+      result[element.name] = element.residents.length;
+    });
+    return result;
+  }
+  const specie = species.find((element) => element.name === animal.specie);
+  if (animal.sex !== undefined) {
+    return specie.residents.filter((element) => element.sex === animal.sex).length;
+  }
+  return specie.residents.length;
 }
-
 module.exports = countAnimals;
